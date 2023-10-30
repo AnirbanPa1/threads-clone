a7674596e4e97dc0c5d9b789a550b52c418daa09
AnirbanPa1 <palanirban2003@gmail.com>
2023-10-30 18:30:29 +0530

    first commit

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -3,6 +3,7 @@ import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+
 async function Page() {
     const user = await currentUser();
 
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -18,6 +18,7 @@ import { usePathname, useRouter } from "next/navigation";
 // import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from '@clerk/nextjs';
 
 interface Props {
   userId: string
@@ -29,6 +30,7 @@ function PostThread({ userId }: Props ) {
     
     const router = useRouter();
     const pathname = usePathname();
+    const { organization } = useOrganization();
 
     const form = useForm({
         resolver: zodResolver(ThreadValidation),
@@ -42,7 +44,7 @@ function PostThread({ userId }: Props ) {
         await createThread({ 
             text: values.thread,
             author: userId,
-            communityId: null,
+            communityId: organization ? organization.id : null,
             path: pathname,
         });
 
